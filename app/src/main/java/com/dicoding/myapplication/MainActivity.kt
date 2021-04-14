bbe11a0f9808601e87e612287c415de61ec70ca5
TriCodeIT <bayu165kusuma@gmail.com>
2021-04-14 08:19:21 +0700

    Project Starter Mobile App Kotlin

---
+++ app/src/main/java/com/dicoding/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.dicoding.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    var data = "Hello World"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+fun onClick() {
+
+}
+
